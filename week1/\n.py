7ac0de6e294f4458812499c7dc4eef925c0caf72
WangYue1998 <47070797+WangYue1998@users.noreply.github.com>
2019-03-09 21:27:08 -0500

    Add files via upload

---
+++ week1/\n.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 18:19:19 2017
+
+@author: wy
+"""
+
+a="I never \n do it"
+print(a)
---
+++ week1/week1first program.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 19:01:29 2017
+
+@author: wy
+"""
+
+x,y,z=0.0, 23.5, 0.0
+a,b = 8,19
+s,t ='ta','da'
+
+import math
+math.sqrt((x-y)**2+(a-b)**2)
